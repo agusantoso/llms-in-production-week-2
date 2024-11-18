691060ca8a4e684a64fdbefbfc319bfce00243e4
alie <agus.lie@sony.com>
2024-11-18 21:57:01 +0800

    add semantic cache and exact match cache on redis

--- src/cached_resources.py
+++ src/cached_resources.py
@@ -1,10 +1,13 @@
 import guardrails as gd
 import streamlit as st
+import redis
 
+from redisvl.extensions.llmcache import SemanticCache
 from src.models import LLMResponse
 from src.prompt import PROMPT
 from phoenix.otel import register
 from openinference.instrumentation.openai import OpenAIInstrumentor
+
 
 @st.cache_resource
 def instrument() -> None:
@@ -18,8 +21,27 @@ def instrument() -> None:
     OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
 
 @st.cache_resource
+def get_semantic_cache() -> SemanticCache:
+    """
+    Create a cache using RedisVL SemanticCache.
+    """
+    llmcache = SemanticCache(
+        name="llmcache",
+        prefix="llmcache",
+        redis_url="redis://redis:6379",
+        distance_threshold=0.1
+    )
+    return llmcache
+
+@st.cache_resource
 def get_guard() -> gd.Guard:
     """
     Create an output guard using GuardRails.
     """
     return gd.Guard.from_pydantic(output_class=LLMResponse, prompt=PROMPT)
+
+def get_exact_match_cache() -> redis.Redis:
+    """
+    Create a Redis connection for exact match cache using Redis Hash Map.
+    """
+    return redis.Redis(host='redis', port=6379, db=0)
