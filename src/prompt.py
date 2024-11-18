6084af3023e195f99ecb782080baaf26f6bb6690
alie <agus.lie@sony.com>
2024-11-18 21:53:00 +0800

    update prompt template

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,9 @@
 PROMPT = """
 Generate a valid SQL query for the following natural language instruction:
 
-Query: ${query}
+${query}
+
+Only generate SQL code and nothing else.
 
 ${gr.complete_json_suffix_v3}
 """
